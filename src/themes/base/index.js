fd1ee50017052c91cc99b75217a10e7d7749eba8
Ademir Besirovic <ademir.besirovic1996@gmail.com>
2019-05-17 01:51:57 +0200

    remove unused colors

--- src/themes/base/index.js
+++ src/themes/base/index.js
@@ -3,16 +3,7 @@ export default {
     primary: '#7D4CDB',
     secondary: '#FD6FFF',
     light: '#EDEDED',
-    light100: '#F8F8F8',
-    light200: '#F2F2F2',
-    light300: '#EDEDED',
-    dark: '#333',
-    dark100: '#444',
-    dark200: '#555',
-    dark300: '#777',
-    dark400: '#999',
-    active: 'rgba(221, 221, 221, 0.5)',
-    default: 'transparent'
+    dark: '#333'
   },
   spacing: {
     small: 4,
