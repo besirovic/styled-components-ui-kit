8e4cc8b14809087f719066822d3173fe03c85399
Ademir Besirovic <ademir.besirovic1996@gmail.com>
2019-05-16 23:56:00 +0200

    redefine theme object structure

--- src/themes/base/index.js
+++ src/themes/base/index.js
@@ -1,5 +1,32 @@
-import button from './button';
-
 export default {
-  button
+  colors: {
+    primary: '#7D4CDB',
+    secondary: '#FD6FFF',
+    light: '#EDEDED',
+    light100: '#F8F8F8',
+    light200: '#F2F2F2',
+    light300: '#EDEDED',
+    dark: '#333',
+    dark100: '#444',
+    dark200: '#555',
+    dark300: '#777',
+    dark400: '#999',
+    active: 'rgba(221, 221, 221, 0.5)',
+    default: 'transparent'
+  },
+  spacing: {
+    small: 4,
+    medium: 8,
+    large: 12
+  },
+  borderSize: {
+    small: 1,
+    medium: 2,
+    large: 4
+  },
+  edgeSize: {
+    small: 2,
+    medium: 4,
+    large: 6
+  }
 };
