af71b6124b2735ddb1310479175281c64071d21d
Ademir Besirovic <ademir.besirovic1996@gmail.com>
2019-05-18 01:15:04 +0200

    remove old global style decorator

--- src/components/GlobalStyle/GlobalStyle.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from 'styled-components';
-
-const GlobalStyle = createGlobalStyle`
-  #root * {
-    font-family: Menlo,'Courier New',Courier,monospace;
-  }
-`;
-
-export default GlobalStyle;
